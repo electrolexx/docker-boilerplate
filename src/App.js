0119977a9a4e97e8fa72289d5aa98091253c0f5b
Alex Desharnais <electrolexx@mac.com>
2020-01-04 20:52:25 -0800

    App.js text update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Boom</p>
+        <p>Testing</p>
         <a
           className="App-link"
           href="https://reactjs.org"
